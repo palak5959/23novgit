53775672002cc2b0c638318d07f83982d87e5c05
palak5959 <palak1401.be23@chitkara.edu.in>
2025-09-02 18:33:46 +0000

    3 sep

---
+++ basic/04_comparisons.js
@@ -0,0 +1,28 @@
+//Basic conversions
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//fasne vale cases jisme datatype hi same nhi he 
+
+// console.log("2">1);
+// console.log("02">1);//automatically convert string to number 
+
+//YEH VALE HUMME NHI KARNE CHAHIYE -> boht confusing->
+
+//convert karne me isssue aaata he kyi baar null covert hota he NAN me or kyi baaar 0 me ..->INCONSISTENCE
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);    
+console.log(undefined>=0);
+
+//=== strictly check krne ke liye 
+
+
+
